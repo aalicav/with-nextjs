7b51f98a2b2cd7a4c01415881523a58ba38a3abc
ali-niuco <alissonoliveira@niuco.com.br>
2024-09-16 10:35:59 -0300

    Refactor layout component: Remove GitHubBanner component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
       <body>
         <Suspense>
           <AntdRegistry>
-            <GitHubBanner />
             <RefineKbarProvider>
               <ColorModeContextProvider defaultMode={theme?.value}>
                 <DevtoolsProvider>
